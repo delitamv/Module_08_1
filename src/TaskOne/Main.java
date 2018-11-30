355dc6a6eb03f67766ea6c13ea2f0f990bb66348
Marina Delita <delita@ukr.net>
2018-11-30 18:55:22 +0200

    Alteration

--- src/TaskOne/Main.java
+++ src/TaskOne/Main.java
@@ -1,28 +1,46 @@
 package TaskOne;
 
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
+        float a = 0;
+        float b = 0;
         System.out.print("Enter a number A: ");
-        float a = scanner.nextFloat();
-        scanner.nextLine();
-        System.out.print("Enter a number B: ");
-        float b = scanner.nextFloat();
-        scanner.nextLine();
-        System.out.print("Enter the character of one of the following operations:" +
-                        " ( \"+\" ; \"-\" ; \"*\" ; \"/\" ; \"%\" ; \"==\" ; \"<\" ; \">\" ) ");
-        String operation = scanner.nextLine();
-        Operations opr = new Operations();
-        if (operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/") || operation.equals("%")) {
-            OperationFloat operationFloat = new OperationFloat(operation, a, b);
-            System.out.println(a + " " + " " + operation + " " + b + " = " + opr.getResult(operationFloat));
-        } else if (operation.equals("==") || operation.equals(">") || operation.equals("<")) {
-            OperationBoolean operationBoolean = new OperationBoolean(operation, a, b);
-            System.out.println(a + " " + " " + operation + " " + b + " = " + opr.getResult(operationBoolean));
-        } else {
-            System.out.println("Error. Bye!!!");
+        for (int i = 0; i < 1; i++) {
+            if (scanner.hasNextFloat()) {
+                a = scanner.nextFloat();
+            } else {
+                System.out.print("You did not enter an integer.");
+                break;
+            }
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number B: ");
+            if (sc.hasNextFloat()) {
+                b = sc.nextFloat();
+            } else {
+                System.out.print("You did not enter an integer. ");
+                break;
+            }
+
+            scanner.nextLine();
+            System.out.print("Enter the character of one of the following operations:" +
+                    " ( \"+\" ; \"-\" ; \"*\" ; \"/\" ; \"%\" ; \"==\" ; \"<\" ; \">\" ) ");
         }
-    }
+        String operation = scanner.nextLine();
+
+        Operations opr = new Operations();
+
+            if (operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/") || operation.equals("%")) {
+                OperationFloat operationFloat = new OperationFloat(operation, a, b);
+                System.out.println(a + " " + " " + operation + " " + b + " = " + opr.getResult(operationFloat));
+            } else if (operation.equals("==") || operation.equals(">") || operation.equals("<")) {
+                OperationBoolean operationBoolean = new OperationBoolean(operation, a, b);
+                System.out.println(a + " " + " " + operation + " " + b + " = " + opr.getResult(operationBoolean));
+            } else {
+                System.out.println("Error. Bye!!!");
+            }
+        }
 }
--- src/TaskOne/Operations.java
+++ src/TaskOne/Operations.java
@@ -4,42 +4,34 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class Operations {
-    public float getResult(OperationFloat operationFloat) {
+    public float getResult(OperationFloat operationFloat) throws ExecutionException, InterruptedException {
         FutureTask<Float> futureTask = new FutureTask<>(operationFloat);
         Thread thread = new Thread(futureTask);
         System.out.println("Thread started. ");
         thread.start();
         float result = 0;
-        try {
+
             result = futureTask.get();
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+//            Thread.sleep(2000);
+
         System.out.println("Thread finished. ");
         return result;
     }
 
-    public boolean getResult(OperationBoolean operationBoolean) {
+    public boolean getResult(OperationBoolean operationBoolean) throws ExecutionException, InterruptedException {
         FutureTask<Boolean> futureTask = new FutureTask<>(operationBoolean);
         Thread thread = new Thread(futureTask);
         System.out.println("Thread started. ");
         thread.start();
         boolean result = false;
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
             result = futureTask.get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+
         System.out.println("Thread finished. ");
         return result;
     }
